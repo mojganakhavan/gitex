7178c55eaad7f0dd51818eda30acdc0625029576
Mojgan Akhavan <Mojgan@Mojgans-MacBook-Pro.local>
2016-10-04 13:07:44 +0200

    first commit

---
+++ sleepIn.java
@@ -0,0 +1,15 @@
+
+public class sleepIn {
+
+	public static void main(String[] args) {
+		sleepIn(false,false);
+	}
+		public static boolean sleepIn(boolean weekday, boolean vacation) {
+			if (!weekday || vacation) {
+				return true;
+			}
+  
+			return false;
+		}
+
+}
